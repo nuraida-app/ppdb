2d1c81e480186403d043f7ab8d2bfa6873fbeeb1
nuraida-app <nuraida.appdev@gmail.com>
2025-01-10 14:51:11 +0700

    Add server connection

--- config/config.js
+++ config/config.js
@@ -3,22 +3,22 @@ import pkg from "pg";
 const { Client } = pkg;
 
 // Development
+// const config = {
+//   user: "postgres",
+//   password: "13090122",
+//   host: "localhost",
+//   database: "ppdb",
+//   port: 5432,
+// };
+
+// Production
 const config = {
   user: "postgres",
-  password: "13090122",
-  host: "localhost",
+  password: "nibs2024*",
+  host: "103.245.38.61",
   database: "ppdb",
   port: 5432,
 };
-
-// Production
-// const config = {
-//   user: "postgres",
-//   password: "nibs2024*",
-//   host: "103.245.38.61",
-//   database: "nibs_ppdb",
-//   port: 5432,
-// };
 
 const client = new Client(config);
 
