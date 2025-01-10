8eb60ca3c6ba9cef8b276e0c28f13963405968cc
Al Magribi <almagribi.appdev@gmail.com>
2025-01-10 17:40:58 +0700

    add build

---
+++ dist/assets/Spinner-CIKaKhhc.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-E7E3HHSK.js";const n=({color:r})=>s.jsx("div",{className:`spinner-border ${r}`,role:"status",children:s.jsx("span",{className:"visually-hidden",children:"Loading..."})});export{n as S};
---
+++ dist/assets/Statistik-DZTbKqiD.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-E7E3HHSK.js";import{L as r}from"./Layout-tmITzfKy.js";const o=()=>t.jsx(r,{children:"Statistik"});export{o as default};
